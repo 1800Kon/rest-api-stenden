1ea1520242f4e6dc1e54592d3a939fb8e3c617e7
Konstan <55129891+KBG360@users.noreply.github.com>
2021-05-30 13:07:26 +0200

    Initial copy from old repo

---
+++ rest-api-stenden/src/main/java/com/example/restapistenden/RestApiStendenApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapistenden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStendenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStendenApplication.class, args);
+	}
+
+}
